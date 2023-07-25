eee0b77355aeedc2e3de6c211dd88f27417dc905
PigTurtleDev <anjosoerries@gmail.com>
2023-07-25 17:56:12 +0200

    starbuncle typos continue, now maybe 0.1.3?

--- overrides/kubejs/server_scripts/compatRecipes.js
+++ overrides/kubejs/server_scripts/compatRecipes.js
@@ -749,7 +749,7 @@ onEvent('recipes', event => {
     ]
     event.replaceInput({id: "create:crafting/kinetics/train_door"},"create:brass_sheet","create:sturdy_sheet")
     event.remove({id: "ars_creo:starbuncle_wheel"})
-    event.shaped("ars_creo:starbuncle_wheel", ["XXX", "XWX", "XXX"], {X:"ars_creo:starbuncle_shard", W:"create:water_wheel"})
+    event.shaped("ars_creo:starbuncle_wheel", ["XXX", "XWX", "XXX"], {X:"ars_nouveau:starbuncle_shards", W:"create:water_wheel"})
     event.replaceInput({id: "create:crafting/kinetics/train_trapdoor"},"create:brass_sheet","create:sturdy_sheet")
     //event.recipes.create.deploying('ars_nouveau:magebloom_crop',['#forge:seeds','kubejs:runic_tablet'])
     multicut(radiant_cutting,"kubejs:radiant_machine",true)
