ca78c0f1a2f6de057ebf9eb302b2b0b215834cb1
PigTurtleDev <110937217+PigTurtleDev@users.noreply.github.com>
2023-07-27 16:11:07 +0200

    fixed M3 dependencies again, stellaFix.js now works as intended without errors

--- overrides/kubejs/server_scripts/stellaFix.js
+++ overrides/kubejs/server_scripts/stellaFix.js
@@ -2,7 +2,7 @@
 // Items with an eternal stella applied break in recipes. Hacky fix to also apply unbreakable to those items when closing a smithing table GUI
 
 onEvent('player.inventory.closed', event =>{
-	//if (event.inventoryContainer.type.registryName != "minecraft:smithing") {return} //Bunch of errors, if anyone knows a way to check for an inventory please let me know
+	if (event.inventoryContainer.getClass().getSimpleName() != "SmithingMenu") {return}
 	event.player.inventory.minecraftInventory.forEach(item =>{
 		if (Item.of(item).getNbtString().includes("Repair:1b") && !Item.of(item).getNbtString().includes("Unbreakable:1b")) {
 			let data = Item.of(item).nbt
