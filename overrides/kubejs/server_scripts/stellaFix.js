419597774e677fdf5553c057e779622d5b50fbd6
PigTurtleDev <110937217+PigTurtleDev@users.noreply.github.com>
2023-08-08 19:46:31 +0200

    fixed the stellaFix to also work for damaged tools

--- overrides/kubejs/server_scripts/stellaFix.js
+++ overrides/kubejs/server_scripts/stellaFix.js
@@ -4,7 +4,7 @@
 onEvent('player.inventory.closed', event =>{
 	if (event.inventoryContainer.getClass().getSimpleName() != "SmithingMenu") {return}
 	event.player.inventory.minecraftInventory.forEach(item =>{
-		if (Item.of(item).getNbtString().includes("Repair:1b") && !Item.of(item).getNbtString().includes("Unbreakable:1b")) {
+		if (Item.of(item).getNbtString().includes("Modifier:\"forbidden_arcanus:eternal\"") && !Item.of(item).getNbtString().includes("Unbreakable:1b")) {
 			let data = Item.of(item).nbt
 			data.Unbreakable = true
 			Item.of(item).setNbt(data)
