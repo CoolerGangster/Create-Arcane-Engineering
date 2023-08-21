be906836dc6da6c25bd196bd36600469141a0b17
CoolerGangster <106472985+CoolerGangster@users.noreply.github.com>
2023-08-21 20:25:25 +0200

    Give skystone loot lol

--- overrides/kubejs/server_scripts/blockLoottables.js
+++ overrides/kubejs/server_scripts/blockLoottables.js
@@ -4,4 +4,7 @@ onEvent("lootjs", (event) => {
     event
         .addBlockLootModifier(/waystones\:.*waystone/)
         .removeLoot(/waystones\:.*waystone/)
+    event
+        .addBlockLootModifier('cae:sky_stone')
+        .removeLoot('ae2:sky_stone_block')
 })
