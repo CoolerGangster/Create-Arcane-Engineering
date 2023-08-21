225684f8d404d3053072909d1b33275d3073df70
PigTurtleDev <110937217+PigTurtleDev@users.noreply.github.com>
2023-08-21 15:55:37 +0200

    small change to soulsand information page

--- overrides/kubejs/client_scripts/jeiItemInfo.js
+++ overrides/kubejs/client_scripts/jeiItemInfo.js
@@ -1,5 +1,5 @@
 onEvent('jei.information', event => {
-    event.add('minecraft:soul_sand', ['Retrieving Soulsand of its Soul', 'Due to a weakening of the arcane bulk haunting sand only yields soulless sand, to reinfuse it with a soul have a Wraith stand on top of it'])
+    event.add('minecraft:soul_sand', ['Retrieving Soulsand of its Soul', '', 'Due to a weakening of the arcane bulk haunting sand only yields soulless sand, to reinfuse it with a soul have a Wraith stand on top of it'])
     event.add('forbidden_arcanus:soulless_sand', ['Retrieving Soulsand of its Soul', 'Due to a weakening of the arcane bulk haunting sand only yields soulless sand, to reinfuse it with a soul have a Wraith stand on top of it '])
     event.add(/sophisticatedbackpacks:.*backpack/, "Upgradable by placing it (Shift+Right Click) and Right Clicking it with the items mentioned on its tooltip.")
 })
