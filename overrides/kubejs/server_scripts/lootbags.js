7a748282a95a75c36529182cd5e19a9c8e153a17
PigTurtleDev <110937217+PigTurtleDev@users.noreply.github.com>
2023-10-04 20:24:48 +0200

    (by Willownezz) better Lootbag scaling for Fortune now works for Fortune levels greater than 3

--- overrides/kubejs/server_scripts/lootbags.js
+++ overrides/kubejs/server_scripts/lootbags.js
@@ -1,31 +1,26 @@
-onEvent('item.right_click',event=>{
-    function lootbag(lootbag,lootable){
-        if(event.item == lootbag){
-            event.cancel()
-            event.player.swingArm(event.hand)
-            event.server.runCommandSilent(`execute in ${event.level.dimension} run loot spawn ${event.player.x} ${event.player.y} ${event.player.z} loot ${lootable}`) 
-            event.player.playSound('minecraft:entity.experience_orb.pickup')
-            if (event.item.hasEnchantment('minecraft:fortune',1) && lootbag != 'kubejs:lootbag_poggers'){
-                
-                event.server.runCommandSilent(`execute in ${event.level.dimension} run loot spawn ${event.player.x} ${event.player.y} ${event.player.z} loot ${lootable}`)
-                if (event.item.hasEnchantment('minecraft:fortune',2)){
-                    
-                    event.server.runCommandSilent(`execute in ${event.level.dimension} run loot spawn ${event.player.x} ${event.player.y} ${event.player.z} loot ${lootable}`)
-                    if(event.item.hasEnchantment('minecraft:fortune',3)){
-                        
-                        event.server.runCommandSilent(`execute in ${event.level.dimension} run loot spawn ${event.player.x} ${event.player.y} ${event.player.z} loot ${lootable}`)
-                    }
-                }
-            }
-            event.item.count--
+function lootbag(lootbag, lootable) {
+    if (event.item == lootbag) {
+        event.cancel();
+        event.player.swingArm(event.hand);
+        event.server.runCommandSilent(`execute in ${event.level.dimension} run loot spawn ${event.player.x} ${event.player.y} ${event.player.z} loot ${lootable}`);
+        event.player.playSound('minecraft:entity.experience_orb.pickup');
+
+        for (let i = 1; i <= 256; i++) {
+            if (event.item.hasEnchantment('minecraft:fortune', i)) {
+                event.server.runCommandSilent(`execute in ${event.level.dimension} run loot spawn ${event.player.x} ${event.player.y} ${event.player.z} loot ${lootable}`);
+            } else break
         }
+        event.item.count--;
     }
-    lootbag('kubejs:lootbag_poggers','loot:patience')
-    lootbag('kubejs:lootbag_create_andesite','loot:lootbag/create/andesite')
-    lootbag('kubejs:lootbag_create_brass','loot:lootbag/create/brass')
-    lootbag('kubejs:lootbag_create_radiant','loot:lootbag/create/radiant')
-    lootbag('kubejs:lootbag_pcb', 'loot:lootbag/create/pcb')
-    lootbag('kubejs:lootbag_arcane_1','cae:arcaneweak')
-    lootbag('kubejs:lootbag_arcane_2','cae:arcanenormal')
-    lootbag('kubejs:lootbag_arcane_3','cae:arcanetable')
-})
+}
+
+onEvent('item.right_click', event => {
+    lootbag('kubejs:lootbag_poggers', 'loot:patience');
+    lootbag('kubejs:lootbag_create_andesite', 'loot:lootbag/create/andesite');
+    lootbag('kubejs:lootbag_create_brass', 'loot:lootbag/create/brass');
+    lootbag('kubejs:lootbag_create_radiant', 'loot:lootbag/create/radiant');
+    lootbag('kubejs:lootbag_pcb', 'loot:lootbag/create/pcb');
+    lootbag('kubejs:lootbag_arcane_1', 'cae:arcaneweak');
+    lootbag('kubejs:lootbag_arcane_2', 'cae:arcanenormal');
+    lootbag('kubejs:lootbag_arcane_3', 'cae:arcanetable');
+});
