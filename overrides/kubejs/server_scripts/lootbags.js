b746bedda732c9f411c36b0f3ba9811cfa31bdfb
PigTurtleDev <110937217+PigTurtleDev@users.noreply.github.com>
2023-10-04 20:54:23 +0200

    (another) Loottable hotfix Yep, i did it again. Now it works.

--- overrides/kubejs/server_scripts/lootbags.js
+++ overrides/kubejs/server_scripts/lootbags.js
@@ -1,25 +1,30 @@
-function lootbag(lootbag, lootable) {
-    if (event.item == lootbag) {
-        event.cancel();
-        event.player.swingArm(event.hand);
-        event.player.playSound('minecraft:entity.experience_orb.pickup');
-
-        for (let i = 0; i <= 256; i++) {
-            if (event.item.hasEnchantment('minecraft:fortune', i)) {
-                event.server.runCommandSilent(`execute in ${event.level.dimension} run loot spawn ${event.player.x} ${event.player.y} ${event.player.z} loot ${lootable}`);
-            } else break
-        }
-        event.item.count--;
-    }
-}
-
 onEvent('item.right_click', event => {
-    lootbag('kubejs:lootbag_poggers', 'loot:patience');
-    lootbag('kubejs:lootbag_create_andesite', 'loot:lootbag/create/andesite');
-    lootbag('kubejs:lootbag_create_brass', 'loot:lootbag/create/brass');
-    lootbag('kubejs:lootbag_create_radiant', 'loot:lootbag/create/radiant');
-    lootbag('kubejs:lootbag_pcb', 'loot:lootbag/create/pcb');
-    lootbag('kubejs:lootbag_arcane_1', 'cae:arcaneweak');
-    lootbag('kubejs:lootbag_arcane_2', 'cae:arcanenormal');
-    lootbag('kubejs:lootbag_arcane_3', 'cae:arcanetable');
+    let lootbags = [
+        ['kubejs:lootbag_poggers', 'loot:patience'],
+        ['kubejs:lootbag_create_andesite', 'loot:lootbag/create/andesite'],
+        ['kubejs:lootbag_create_brass', 'loot:lootbag/create/brass'],
+        ['kubejs:lootbag_create_radiant', 'loot:lootbag/create/radiant'],
+        ['kubejs:lootbag_pcb', 'loot:lootbag/create/pcb'],
+        ['kubejs:lootbag_arcane_1', 'cae:arcaneweak'],
+        ['kubejs:lootbag_arcane_2', 'cae:arcanenormal'],
+        ['kubejs:lootbag_arcane_3', 'cae:arcanetable'],
+    ]
+
+    lootbags.forEach(bag => {
+        let lootbag = bag[0]
+        let loottable = bag[1]
+
+        if (event.item == lootbag) {
+            event.cancel();
+            event.player.swingArm(event.hand);
+            event.player.playSound('minecraft:entity.experience_orb.pickup');
+            const {x, y, z} = event.player
+            for (let i = 0; i <= 256; i++) {
+                if (event.item.hasEnchantment('minecraft:fortune', i)) {
+                    event.server.runCommandSilent(`execute in ${event.level.dimension} run loot spawn ${x} ${y} ${z} loot ${loottable}`);
+                } else break
+            }
+            event.item.count--;
+        }
+    })
 });
