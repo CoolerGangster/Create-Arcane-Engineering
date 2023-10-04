789ed37c4fde18ce6cf49d8e17d82caf4c654f92
Troy Siedsma <tsiedsma@lithiumhosting.com>
2023-10-04 07:59:06 -0500

    Fix Integrated Dynamics script and cleanup recipes Ensure all parts can be crafted back to themselves without NBT data This does not allow crafting blocked / removed parts, but if the part exists it can still be used.

--- overrides/kubejs/server_scripts/integrated_dynamics.js
+++ overrides/kubejs/server_scripts/integrated_dynamics.js
@@ -1,16 +1,59 @@
 /**
  * Simple recipe changes to reset IDs on parts.
  */
+
+let integrated_parts = [
+    'integrateddynamics:part_audio_reader',
+    'integrateddynamics:part_block_reader',
+    'integrateddynamics:part_entity_reader',
+    'integrateddynamics:part_extradimensional_reader',
+    'integrateddynamics:part_fluid_reader',
+    'integrateddynamics:part_inventory_reader',
+    'integrateddynamics:part_machine_reader',
+    'integrateddynamics:part_network_reader',
+    'integrateddynamics:part_redstone_reader',
+    'integrateddynamics:part_world_reader',
+    'integrateddynamics:part_audio_writer',
+    'integrateddynamics:part_effect_writer',
+    'integrateddynamics:part_entity_writer',
+    'integrateddynamics:part_machine_writer',
+    'integrateddynamics:part_inventory_writer',
+    'integrateddynamics:part_redstone_writer',
+    'integrateddynamics:part_static_light_panel',
+    'integrateddynamics:part_dynamic_light_panel',
+    'integrateddynamics:part_display_panel',
+    'integrateddynamics:part_connector_mono_directional',
+    'integratedterminals:part_terminal_storage',
+    'integratedterminals:part_terminal_crafting_job',
+    'integratedtunnels:part_interface_energy',
+    'integratedtunnels:part_interface_filter_energy',
+    'integratedtunnels:part_importer_energy',
+    'integratedtunnels:part_exporter_energy',
+    'integratedtunnels:part_interface_item',
+    'integratedtunnels:part_interface_filter_item',
+    'integratedtunnels:part_importer_item',
+    'integratedtunnels:part_exporter_item',
+    'integratedtunnels:part_interface_fluid',
+    'integratedtunnels:part_interface_filter_fluid',
+    'integratedtunnels:part_importer_fluid',
+    'integratedtunnels:part_exporter_fluid',
+    'integratedcrafting:part_interface_crafting',
+    'integratedcrafting:part_crafting_writer'
+]
 onEvent('recipes', event => {
+    event.remove({output: 'integrateddynamics:logic_director'})
+    event.remove({output: 'integrateddynamics:energy_battery'})
+    event.remove({output: 'integratedcrafting:part_interface_crafting'})
+    event.remove({output: 'integratedcrafting:part_crafting_writer'})
+    event.remove({output: /integrateddynamics\:part.*porter_world/})
+    event.remove({output: /integratedtunnels\:part.*porter_world/})
+    event.remove({output: "integratedtunnels:part_player_simulator"})
+    event.remove({output: 'integrateddynamics:part_connector_mono_directional'})
+    event.remove({output: "integrateddynamics:part_connector_omni_directional"})
+    event.remove({output: "integratedterminals:terminal_storage_portable"})
+    event.remove({output: 'integrateddynamics:coal_generator'})
 
-    // Fix Integrated Tunnels items not able to be reset once an ID is defined.
-    event.shapeless('integratedtunnels:part_interface_item', ['integratedtunnels:part_interface_item'])
-    event.shapeless('integratedtunnels:part_importer_item', ['integratedtunnels:part_importer_item'])
-    event.shapeless('integratedtunnels:part_exporter_item', ['integratedtunnels:part_exporter_item'])
-    event.shapeless('integratedtunnels:part_interface_fluid', ['integratedtunnels:part_interface_fluid'])
-    event.shapeless('integratedtunnels:part_importer_fluid', ['integratedtunnels:part_importer_fluid'])
-    event.shapeless('integratedtunnels:part_exporter_fluid', ['integratedtunnels:part_exporter_fluid'])
-
-    // Fix Integrated Dynamics items not able to be reset once an ID is defined.
-    event.shapeless('integrateddynamics:part_connector_mono_directional', ['integrateddynamics:part_connector_mono_directional'])
+    integrated_parts.forEach(part => {
+        event.shapeless(part, [part])
+    })
 })
