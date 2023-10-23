a26ef14fcb4d2740aee2f85098873bf8b4842d15
CoolerGangster <106472985+CoolerGangster@users.noreply.github.com>
2023-10-23 13:54:42 +0200

    added oil multiblocks (they dont work yet) and made ch 3 cheaper

--- overrides/kubejs/server_scripts/draining.js
+++ overrides/kubejs/server_scripts/draining.js
@@ -1,5 +1,5 @@
 onEvent('recipes', event => {
     event.recipes.createEmptying(['kubejs:chrom',Fluid.of('kubejs:chromatic_waste', 250)], 'minecraft:glowstone_dust')
-    event.recipes.createEmptying(['kubejs:empty_vial',Fluid.of('kubejs:liquid_anthraquinone', 36)], 'kubejs:anthraquinone')
+    event.recipes.createEmptying(['kubejs:empty_vial',Fluid.of('kubejs:liquid_anthraquinone', 56)], 'kubejs:anthraquinone')
     event.recipes.createEmptying(['kubejs:dirty_bowl',Fluid.of('kubejs:impure_source', 50)], 'kubejs:magebloom_sourceberry_smoothie_bowl')
 })
