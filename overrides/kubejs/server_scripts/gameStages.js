3115112c0784e5cebb56338702a67002936a92e0
darklotus781 <35048745+darklotus781@users.noreply.github.com>
2023-10-24 11:39:32 -0500

    Update gameStages.js
    Removed debugging / testing stuff.

--- overrides/kubejs/server_scripts/gameStages.js
+++ overrides/kubejs/server_scripts/gameStages.js
@@ -1,11 +1,9 @@
 onEvent('server.load', event => {
     event.server.players.forEach(p => {
-        p.stages.remove('arcane_crafting');
         if (p.stages.has('arcane_crafting')) {
             p.sendData('jei_show_stage_one', {})
         }
 
-        p.stages.remove('keystone_crafting');
         if (p.stages.has('keystone_crafting')) {
             p.sendData('jei_show_stage_two', {})
         }
@@ -13,12 +11,10 @@ onEvent('server.load', event => {
 })
 
 onEvent('player.logged_in', event => {
-    event.player.stages.remove('arcane_crafting');
     if (event.player.stages.has('arcane_crafting')) {
         event.player.sendData('jei_show_stage_one', {})
     }
 
-    event.player.stages.remove('keystone_crafting');
     if (event.player.stages.has('keystone_crafting')) {
         event.player.sendData('jei_show_stage_two', {})
     }
